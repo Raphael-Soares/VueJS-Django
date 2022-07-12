cd9f360a3a8dcb40461360abc371328efb99c5d0
Raphael-Soares <soaresphaell@gmail.com>
2022-07-12 09:20:13 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+ <h1>Livros</h1>
+ <div v-for="livro of livros" :key="livro.id">
+ <h2>{{livro.titulo}} || {{livro.preco}}</h2>
+ <h3>{{livro.categoria}} || {{livro.editora}}</h3>
+ </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      livros: ''
+    }
+  },
+  mounted () {
+     axios({
+            method:'get',
+            url: 'http://127.0.0.1:8000/livros/',
+            auth: {
+                username: 'Raphael',
+                password: 'gamemode'
+            }
+        }).then(response => this.livros = response.data);
+  }
+}
+</script>
+
+<style>
+
+</style>
